e124d1a6a63e03acbd97da770f55c8a1505410db
Michal Gritzbach <gritzbach.michal@gmail.com>
2020-02-12 12:37:19 +0100

    Remove LocaleEnum

--- lib/graphql/schema/directive/locale.rb
+++ lib/graphql/schema/directive/locale.rb
@@ -4,17 +4,11 @@ module GraphQL
   class Schema
     class Directive
       class Locale < GraphQL::Schema::Directive
-        class LocaleEnum < GraphQL::Schema::Enum
-          def self.locale(codename:, description: '')
-            value(codename.underscore, description: description, value: codename)
-          end
-        end
+        locations(
+          GraphQL::Schema::Directive::FIELD
+        )
 
-        locations GraphQL::Schema::Directive::FIELD
-
-        attr_accessor :locale_enum
-
-        argument :lang, type: LocaleEnum, required: false
+        argument :lang, String, required: false
 
         def self.resolve(_object, argument, _context)
           I18n.with_locale(argument[:lang]) do
