603f9cb2ebd3e427e7fbd8ae63c304882a71a965
Kenneth Law <cyt05108@gmail.com>
2020-02-17 11:02:56 +0800

    Revert "Remove LocaleEnum"
    This reverts commit e124d1a6a63e03acbd97da770f55c8a1505410db.

--- lib/graphql/schema/directive/locale.rb
+++ lib/graphql/schema/directive/locale.rb
@@ -4,11 +4,17 @@ module GraphQL
   class Schema
     class Directive
       class Locale < GraphQL::Schema::Directive
-        locations(
-          GraphQL::Schema::Directive::FIELD
-        )
+        class LocaleEnum < GraphQL::Schema::Enum
+          def self.locale(codename:, description: '')
+            value(codename.underscore, description: description, value: codename)
+          end
+        end
 
-        argument :lang, String, required: false
+        locations GraphQL::Schema::Directive::FIELD
+
+        attr_accessor :locale_enum
+
+        argument :lang, type: LocaleEnum, required: false
 
         def self.resolve(_object, argument, _context)
           I18n.with_locale(argument[:lang]) do
